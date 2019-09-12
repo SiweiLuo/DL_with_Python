0fb808a7a79a5f6bb619e2d055a17f4a0985c1f1
Siwei Luo <siuluosiwei@gmail.com>
2019-09-12 02:19:16 -0500

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,51 @@
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np 
+import random
+
+square_size = 1 
+points_inside_circle = 0. 
+points_inside_square = 0. 
+sample_size = 1000000 
+arc = np.linspace(0,np.pi/2,100)
+
+
+def generate_points(size):
+  x = random.random()*size
+  y = random.random()*size
+  print(x,y)
+  return (x,y) 
+
+
+def is_in_circle(point,size):
+  print(math.sqrt(point[0]**2+point[1]**2)<=size)
+  return math.sqrt(point[0]**2+point[1]**2)<=size 
+
+
+def compute_pi(points_inside_circle,points_inside_square):
+  print(points_inside_circle)
+  print(points_inside_square)
+  print(4.*(points_inside_circle/points_inside_square))
+  return 4.*(points_inside_circle/points_inside_square)
+
+
+plt.axes().set_aspect('equal')
+plt.plot(1*np.cos(arc),1*np.sin(arc)) 
+
+for i in range(sample_size):
+  point = generate_points(square_size)
+  plt.plot(point[0],point[1],'c.')
+  points_inside_square+=1
+  if is_in_circle(point,square_size):
+    print(points_inside_circle)
+    points_inside_circle += 1 
+
+print("Approximate value of pi is {}".format(compute_pi(points_inside_circle,points_inside_square)))
+
+
+
+
+
+
+
